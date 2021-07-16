3b82cd599f6af65aef81d54932be15611b080a71
yassi-github <s19t312@stu.kagawa-u.ac.jp>
2021-07-16 19:29:47 +0000

    frontをちょっと見栄え良くした

--- static/js/main.js
+++ static/js/main.js
@@ -15,37 +15,54 @@ async function getApi(url) {
     return await response.json()
 }
 
-let problemTitleElement = document.getElementById('title')
-let problemDescriptionElement = document.getElementById('desc')
+let problemsElement = document.getElementById('problems')
 let resultAreaElement = document.getElementById('result-area')
 
 // get all problems
 getApi('/problems').then(data => {
-    problemTitleElement.innerText = data.title
-    problemDescriptionElement.innerText = data.description
-})
-
-// submit solution
-document.getElementById('send-button').addEventListener('click', () => {
-    let b64ScriptText = btoa(document.getElementById('input-area').value)
-    let answerRequestBody = {
-        "username": "sample",
-        "script": b64ScriptText
-    }
-    let url = location.origin + '/problems/1/answer'
-
-    sendPost(url, answerRequestBody).then(response => {
-        // show command result
-        resultAreaElement.innerHTML = atob(response.result).replace(/\n/g, '<br>')
-        
-        // C or W
-        if (response.is_correct) {
-            document.getElementById('is-correct').innerHTML = 'Correct!'
-        } else {
-            document.getElementById('is-correct').innerHTML = 'Incorrect!'
-        }
-    }, error => {
-        resultAreaElement.innerHTML = error
+    // show problems
+    data.forEach(problem => {
+        let problemElement = document.createElement('div')
+        let problemElementInputareaID = 'input-command-' + problem.id
+        problemElement.className = 'problem'
+        problemElement.id = 'problem-' + problem.id
+        problemElement.innerHTML = `
+            <div class="problem-title">
+                <h3>${problem.title}</h3>
+                <p>${problem.text}</p>
+                <a onclick="" href="problems/${problem.id}/file">No.${problem.id} Question File</a>
+            </div>
+            <div id="input-area">
+                <input autofocus type="text" size="50" id="${problemElementInputareaID}">
+                <button id="send-button-${problem.id}">EXECUTE</button>
+            </div>
+            `
+            problemsElement.appendChild(problemElement)
+            
+            // submit solution
+            document.getElementById(`send-button-${problem.id}`).addEventListener('click', () => {
+                let b64ScriptText = btoa(document.getElementById(problemElementInputareaID).value)
+                let answerRequestBody = {
+                    "username": "sample",
+                    "script": b64ScriptText
+                }
+                let url = location.origin + `/problems/${problem.id}/answer`
+            
+                sendPost(url, answerRequestBody).then(response => {
+                    // show command result
+                    resultAreaElement.innerHTML = atob(response.result).replace(/\n/g, '<br>')
+            
+                    // C or W
+                    if (response.is_correct) {
+                        document.getElementById('is-correct').innerHTML = 'Correct!'
+                    } else {
+                        document.getElementById('is-correct').innerHTML = 'Incorrect!'
+                    }
+                }, error => {
+                    resultAreaElement.innerHTML = error
+                })
+            })
     })
 })
+
 
