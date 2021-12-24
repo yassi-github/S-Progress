24f706c56dcdebae623e56c80ccb457464fbf834
yassi-github <s19t312@kagawa-u.ac.jp>
2021-12-24 20:56:47 +0000

    :sparkles: show prev/next result from json

--- static/js/main.js
+++ static/js/main.js
@@ -15,9 +15,32 @@ async function getApi(url) {
     return await response.json()
 }
 
+function renewResult(jsonStdout, showingResultIdx) {
+    let prevIdx = showingResultIdx - 1
+    let nextIdx = showingResultIdx + 1
+    try {
+        prevResultAreaElement.innerHTML = jsonStdout[prevIdx][prevIdx].join('<br>')
+    } catch (error) {
+        prevResultAreaElement.innerHTML = ''
+    }
+    
+    currentResultAreaElement.innerHTML = jsonStdout[showingResultIdx][showingResultIdx].join('<br>')
+    
+    try {
+        nextResultAreaElement.innerHTML = jsonStdout[nextIdx][nextIdx].join('<br>')
+    } catch (error) {
+        nextResultAreaElement.innerHTML = ''
+    }
+}
+
 let problemsElement = document.getElementById('problems')
 let resultAreaElement = document.getElementById('result-area')
-
+let prevResultAreaElement = document.getElementById('prev-result')
+let currentResultAreaElement = document.getElementById('current-result')
+let nextResultAreaElement = document.getElementById('next-result')
+let isCorrectElement = document.getElementById('is-correct')
+let prevButtonElement = document.getElementById('prev-button')
+let nextButtonElement = document.getElementById('next-button')
 // get all problems
 getApi('/problems').then(data => {
     // show problems
@@ -54,17 +77,41 @@ getApi('/problems').then(data => {
                 if (response.detail) {
                     resultAreaElement.innerHTML = response.detail
                 } else {
-                    resultAreaElement.innerHTML = decodeURIComponent(response.result).replaceAll(/\n/g, '<br>')
+                    // resultAreaElement.innerHTML = decodeURIComponent(response.result).replaceAll(/\n/g, '<br>')
+                    let resultJsonData = decodeURIComponent(response.result)
+                    let jsonStdout = JSON.parse(resultJsonData)['stdout']
+                    let finalCommandResultPhase = jsonStdout.length - 1
+                    // 現在注目している出力のインデックス
+                    let showingResultIdx = finalCommandResultPhase
+                    // show final result
+                    renewResult(jsonStdout, showingResultIdx)
+                    prevButtonElement.addEventListener('click', e => {
+                        // 0 or lower: do nothing
+                        if (showingResultIdx > 0) {
+                            showingResultIdx -= 1
+                            renewResult(jsonStdout, showingResultIdx)
+                        }
+                    })
+                    nextButtonElement.addEventListener('click', e => {
+                        // last array idx or higher: do nothing
+                        if (showingResultIdx < finalCommandResultPhase) {
+                            showingResultIdx += 1
+                            renewResult(jsonStdout, showingResultIdx)
+                        }
+                    })
                 }
 
                 // C or W
                 if (response.is_correct) {
-                    document.getElementById('is-correct').innerHTML = 'Correct!'
+                    isCorrectElement.innerHTML = 'Correct!'
                 } else {
-                    document.getElementById('is-correct').innerHTML = 'Incorrect!'
+                    isCorrectElement.innerHTML = 'Incorrect!'
                 }
             }, error => {
-                resultAreaElement.innerHTML = error
+                // コマンドがおかしくて実行エラーが出た場合、コンテナが死んでしまう。
+                // つまりフロントに返るのは`Internal Server Error`という文字列。
+                resultAreaElement.innerHTML = "Error...<br>The command could not be executed correctly."
+                isCorrectElement.innerHTML = 'Error!'
             })
         })
         // input要素が入力されていてかつ、Enterが押されたら、button要素をクリックする
