396ef8c0da547a9579cce138929cf5dd7a49bee7
yassi-github <s19t312@kagawa-u.ac.jp>
2022-01-01 21:33:48 +0000

    :children_crossing::lipstick: add prev/current/next label

--- static/js/main.js
+++ static/js/main.js
@@ -46,10 +46,14 @@ let isCorrectElement = document.getElementById('is-correct')
 let prevButtonElement = document.getElementById('prev-button')
 let nextButtonElement = document.getElementById('next-button')
 let yourCommandElement = document.getElementById('your-command')
+
+let incorrectedCountArr = []
+
 // get all problems
 getApi('/problems').then(data => {
     // show problems
     data.forEach(problem => {
+        incorrectedCountArr.push(0)
         let problemElement = document.createElement('div')
         let problemElementInputareaID = 'input-command-' + problem.id
         problemElement.className = 'problem'
@@ -60,8 +64,9 @@ getApi('/problems').then(data => {
                 <h3>${problem.title}</h3>
                 <p>${problemTextBr}</p>
                 <a onclick="" href="problems/${problem.id}/file">No.${problem.id} Question File</a>
-                <a href="problems/${problem.id}/hint1">No.${problem.id} Hint 1</a>
-                <a href="problems/${problem.id}/hint2">No.${problem.id} Hint 2</a>
+                <button id="hint1-${problem.id}">Hint 1</button>
+                <button id="hint2-${problem.id}">Hint 2</button>
+                <button id="answer-${problem.id}" style="display: none;">Answer</button>
             </div>
             <div id="input-area">
                 <input autofocus type="text" id="${problemElementInputareaID}" placeholder="cat q_${problem.id}.txt | ..." style="width: 90%;">
@@ -69,6 +74,23 @@ getApi('/problems').then(data => {
             </div>
             `
         problemsElement.appendChild(problemElement)
+
+        // show sample answer and hint
+        document.getElementById(`hint1-${problem.id}`).addEventListener('click', () => {
+            getApi(`/problems/${problem.id}/hint1`).then(data => {
+                alert(data.hint1)
+            })
+        })
+        document.getElementById(`hint2-${problem.id}`).addEventListener('click', () => {
+            getApi(`/problems/${problem.id}/hint2`).then(data => {
+                alert(data.hint2)
+            })
+        })
+        document.getElementById(`answer-${problem.id}`).addEventListener('click', () => {
+            getApi(`/problems/${problem.id}/answer`).then(data => {
+                alert(data.shell)
+            })
+        })
 
         // submit solution
         document.getElementById(`send-button-${problem.id}`).addEventListener('click', () => {
@@ -84,7 +106,7 @@ getApi('/problems').then(data => {
                 // show command result
                 // HTTPExceptionのときはdetailにメッセージが入る
                 if (response.detail) {
-                    resultAreaElement.innerHTML = response.detail
+                    currentResultAreaElement.innerHTML = response.detail
                 } else {
                     // resultAreaElement.innerHTML = decodeURIComponent(response.result).replaceAll(/\n/g, '<br>')
                     let resultJsonData = decodeURIComponent(response.result)
@@ -113,14 +135,26 @@ getApi('/problems').then(data => {
                 // C or W
                 if (response.is_correct) {
                     isCorrectElement.textContent = 'Correct!'
+                    incorrectedCountArr[problem.id - 1] = 0
                 } else {
                     isCorrectElement.textContent = 'Incorrect!'
+                    incorrectedCountArr[problem.id - 1] += 1
+                    // incorrect (or error)が5回になったら，answerボタンが表示される
+                    if (incorrectedCountArr[problem.id - 1] > 4) {
+                        document.getElementById(`answer-${problem.id}`).style.display = ''
+                    }
                 }
             }, error => {
                 // コマンドがおかしくて実行エラーが出た場合、コンテナが死んでしまう。
                 // つまりフロントに返るのは`Internal Server Error`という文字列。
                 currentResultAreaElement.innerHTML = "Error...<br>The command could not be executed correctly."
                 isCorrectElement.innerHTML = 'Error!'
+
+                // エラーの場合もincoreect countをプラス
+                incorrectedCountArr[problem.id - 1] += 1
+                if (incorrectedCountArr[problem.id - 1] > 5) {
+                    document.getElementById(`answer-${problem.id}`).style.display = ''
+                }
             })
         })
         // input要素が入力されていてかつ、Enterが押されたら、button要素をクリックする
